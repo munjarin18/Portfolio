4feaed8debd5e0fc6ff072cc680f46e6670840cd
munjarin <anjumanarju2718@gmail.com>
2024-10-24 00:11:31 +0600

    done

--- src/components/Protfolio.jsx
+++ src/components/Protfolio.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Container from "./Container";
-import Orebi from "../assets/Orebi.png";
+import Orebi from "../assets/Munjarin.jpg";
 import Flex from "./Flex";
 import Slider from "react-slick";
+
 
 
 function SampleNextArrow(props) {
@@ -41,7 +42,7 @@ const Protfolio = () => {
 
   return (
     <>
-      <section className="py-[30px]  ">
+      <section className="py-[30px]   ">
         <Container>
           <div className="">
             <h2 className="font-sans font-extrabold lg:text-[110px] text-center text-[70px] opacity-15 ">
@@ -52,8 +53,33 @@ const Protfolio = () => {
             </h3>
           </div>
 
-          <slider {...settings}>
-            <div className=" flex justify-between space-x-3 py-10">
+          <Slider {...settings}>
+         
+              <div className="">
+                <a
+                  className=' relative  after:absolute after:contain-[""] after:top-0 after:left-0 after:h-[0] after:w-[100%] after:bg-[#72b626] after:opacity-90 after:duration-300 after:ease-in-out after:rounded-2xl after:hover:h-[100%]'
+                  href="#"
+                >
+                  <img className="rounded-2xl" src={Orebi} alt="" />{" "}
+                  <h2 className=" absolute top-[0] left-[0] h-[100%] w-[100%] duration-300 ease-in-out pt-[10%] pl-[20%] z-20 opacity-0 hover:opacity-100 text-[#fff] lg:text-[36px] text-[28px] font-serif font-semibold ">
+                    E-Commerce <h3 className=" text-[#000]">Website </h3>
+                  </h2>
+                </a>
+              </div>
+
+              
+              <div className="">
+                <a
+                  className=' relative  after:absolute after:contain-[""] after:top-0 after:left-0 after:h-[0] after:w-[100%] after:bg-[#72b626] after:opacity-90 after:duration-300 after:ease-in-out after:rounded-2xl after:hover:h-[100%]'
+                  href="#"
+                >
+                  <img className=" rounded-2xl" src={Orebi} alt="" />{" "}
+                  <h2 className=" absolute top-[0] left-[0] h-[100%] w-[100%] duration-300 ease-in-out pt-[10%] pl-[20%] z-20 opacity-0 hover:opacity-100 text-[#fff] lg:text-[36px] text-[28px] font-serif font-semibold ">
+                    E-Commerce <h3 className=" text-[#000]">Website </h3>
+                  </h2>
+                </a>
+              </div>
+              
               <div className="">
                 <a
                   className=' relative  after:absolute after:contain-[""] after:top-0 after:left-0 after:h-[0] after:w-[100%] after:bg-[#72b626] after:opacity-90 after:duration-300 after:ease-in-out after:rounded-2xl after:hover:h-[100%]'
@@ -66,43 +92,13 @@ const Protfolio = () => {
                 </a>
               </div>
 
-              <div className="">
-                <a
-                  className=' relative  after:absolute after:contain-[""] after:top-0 after:left-0 after:h-[0] after:w-[100%] after:bg-[#72b626] after:opacity-90 after:duration-300 after:ease-in-out after:rounded-2xl after:hover:h-[100%]'
-                  href="#"
-                >
-                  <img className=" rounded-2xl" src={Orebi} alt="" />{" "}
-                  <h2 className=" absolute top-[0] left-[0] h-[100%] w-[100%] duration-300 ease-in-out pt-[10%] pl-[20%] z-20 opacity-0 hover:opacity-100 text-[#fff] lg:text-[36px] text-[28px] font-serif font-semibold ">
-                    E-Commerce <h3 className=" text-[#000]">Website </h3>
-                  </h2>
-                </a>
-              </div>
+             
 
-              <div className="">
-                <a
-                  className=' relative  after:absolute after:contain-[""] after:top-0 after:left-0 after:h-[0] after:w-[100%] after:bg-[#72b626] after:opacity-90 after:duration-300 after:ease-in-out after:rounded-2xl after:hover:h-[100%]'
-                  href="#"
-                >
-                  <img className=" rounded-2xl" src={Orebi} alt="" />{" "}
-                  <h2 className=" absolute top-[0] left-[0] h-[100%] w-[100%] duration-300 ease-in-out pt-[10%] pl-[20%] z-20 opacity-0 hover:opacity-100 text-[#fff] lg:text-[36px] text-[28px] font-serif font-semibold ">
-                    E-Commerce <h3 className=" text-[#000]">Website </h3>
-                  </h2>
-                </a>
-              </div>
+             
 
-              <div className="">
-                <a
-                  className=' relative  after:absolute after:contain-[""] after:top-0 after:left-0 after:h-[0] after:w-[100%] after:bg-[#72b626] after:opacity-90 after:duration-300 after:ease-in-out after:rounded-2xl after:hover:h-[100%]'
-                  href="#"
-                >
-                  <img className=" rounded-2xl" src={Orebi} alt="" />{" "}
-                  <h2 className=" absolute top-[0] left-[0] h-[100%] w-[100%] duration-300 ease-in-out pt-[10%] pl-[20%] z-20 opacity-0 hover:opacity-100 text-[#fff] lg:text-[36px] text-[28px] font-serif font-semibold ">
-                    E-Commerce <h3 className=" text-[#000]">Website </h3>
-                  </h2>
-                </a>
-              </div>
-            </div>
-          </slider>
+              
+       
+          </Slider>
         </Container>
       </section>
     </>
