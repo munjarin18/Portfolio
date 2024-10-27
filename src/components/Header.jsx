522809ac9862e97be04a0b47c67390c1df36738b
munjarin <anjumanarju2718@gmail.com>
2024-10-27 17:25:10 +0600

    demo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
           </div>
         </div>
         <div className="w-3/4">
-          <ul className={`lg:flex justify-center lg:space-x-5 lg:static text-center absolute duration-700 ease-in-out ${show == true ? 'bg-slate-300 lg:bg-transparent text-[#fff] items-center top-[60px] left-0 w-full ' : 'top-[100px] left-[-200px]'}`}>
+          <ul className={`lg:flex justify-center lg:space-x-5 lg:static text-center absolute duration-700 lg:duration-0 ease-in-out ${show == true ? 'bg-slate-300 lg:bg-transparent text-[#fff] items-center top-[60px] left-0 w-full ' : 'top-[100px] left-[-200px]'}`}>
           
             <li className="font-sans font-bold lg:text-[18px] text-[16px]
          text-[#262626]   hover:text-[orange]">
